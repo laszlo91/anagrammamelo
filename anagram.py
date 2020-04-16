d89d9168e9948ae9c362c85e4c7664a7ad355429
laszlo91 <bonora91@live.it>
2020-04-16 20:23:33 +0200

    progresses

--- anagram.py
+++ anagram.py
@@ -1,39 +1,67 @@
-def anagram(name):
-    
-    name = [i for i in list(name.lower()) if i not in ["'"," ", ".", "-", ","]]
-
-    with open("morph-it_048_utf8.txt") as file:
+def openmorphit():
+    with open("morphit.txt") as file:
         morphit = [i.split('\t')[0].lower() for i in file.readlines() if i.split('\t')[-1]\
             .startswith(("ADV","VER","ADJ","AUX","INT", "NOUN"))]
-            
-    def contrast1(word,name):
+        return morphit
+
+
+def cleanup(name):    
+    return [i for i in name if i not in ["'"," ", ".", "-", ","]]
+
+    
+def isanagram(word,name):
+    if len(word) != len(name):
+        return False
+    if word == name:
+        return False
+    for letter in word:
+        if name.count(letter) < word.count(letter):
+            return False
+    return True
+
+    
+def filter_candidates(name, morphit):
+        
+    def issublist(word,name):
         for letter in word:
             if letter not in name:
                 return False
             if name.count(letter) < word.count(letter):
                 return False
         return True
-    
-    def contrast2(word,name):
-        if len(word) != len(name):
-            return False
-        for letter in word:
-            if name.count(letter) < word.count(letter):
-                return False
-        return True
         
-    
     morphit_candidates = []
     for word in morphit:
-        if contrast1(list(word),name) is True:
+        w = list(word)
+        if issublist(w,name):
             morphit_candidates.append(word)
-        
-    morphit_candidates = set(morphit_candidates)
+    return morphit_candidates
+
+
+def monogram(name, morphit):
     
+    name = cleanup(name)
+    b = []
+    for i in morphit:
+        if isanagram(list(i), name):
+            b.append(i)
+    return set(b)
+
+    
+def anagram(name, morphit):
+    
+    name = list(name.lower())
+    
+    if " " not in name and len(name) < 12:
+        return monogram(name, morphit)
+    
+    name = cleanup(name)
+            
+    morphit_candidates = set(filter_candidates(name, morphit))
+            
     a=[]
     for w1 in morphit_candidates:
         for w2 in morphit_candidates:
-            if contrast2(list(w1)+list(w2), name) is True:
+            if isanagram(list(w1)+list(w2), name):
                 a.append(w1+" "+w2)
-                
     return a
--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from anagram import anagram
+from anagram import anagram, openmorphit
 import os
 
 project_root = os.path.dirname(__file__)
@@ -14,9 +14,13 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
+    morphit = openmorphit()
     text = request.form['fname']
-    listofanagrams = anagram(text)
-    return render_template("templates/anagrammi.html", data=listofanagrams)
+    listofanagrams = anagram(text, morphit)
+    if len(listofanagrams) == 0:
+        return "Ci spiace, nessun anagramma disponibile per questo nome. Puoi tornare a dedicarti alle cose importanti della tua giornata."
+    else:
+        return render_template("templates/anagrammi.html", data=listofanagrams)
 
 #if __name__ == "__main__":
 #    app.run(debug=True)
